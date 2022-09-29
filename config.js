0dbedec38e0afea89b04ef9ee8bbd2b181768a64
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2022-09-29 16:37:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -30,26 +30,26 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/al_pakel'
+global.sig = 'https://instagram.com/_ctzhid'
 global.sgh = '-' //github
-global.sgc = 'https://chat.whatsapp.com/E1TElUfSJY63Cs9WqNjEEC' //group whatsapp
+global.sgc = 'https://chat.whatsapp.com/Iu9I1k8FnXW1sXT41XIF50' //group whatsapp
 global.sdc = '-' //discord
-global.snh = 'https://youtube.com/channel/UCbI74T0AXUI26VC88NyoWdw' // my youtube channel
+global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
 
 /*============== PAYMENT ==============*/
-global.pdana = '08989513496' //dana
-global.povo = '____' //ovo
-global.pgopay = '08989513496' //gopay
-global.plinkaja = '____' //link aja
-global.ppulsa = '08989513496' //telkomsel
-global.ppulsa2 = '____' // kalau ada kartu lain isi aja
-global.psaweria = '____'
+global.pdana = '08XXXXXXXXXX' //dana
+global.povo = '08XXXXXXXXXX' //ovo
+global.pgopay = '08XXXXXXXXXX' //gopay
+global.plinkaja = '08XXXXXXXXXX' //link aja
+global.ppulsa = '08XXXXXXXXXX' //telkomsel
+global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
+global.psaweria = 'https://saweria.com/mrw4h1d'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '628XXXXXXX'
-global.nomorown = '628989513496'
-global.namebot = 'NeKo-Botzz'
-global.nameown = 'DIA PEDO BANG :v :V'
+global.nomorbot = '628XXXXXXXXXX'
+global.nomorown = '628XXXXXXXXXX'
+global.namebot = 'WH-MODS-BOT-V1'
+global.nameown = 'WH MODS DEV'
 
 /*============== APIKEY SIMPEL ==============*/
 global.zenzkey = 'b8040941f7'
@@ -58,10 +58,10 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['628989513496', 'DIA PEDO BANG ;V', true],
-  ['628989513496', 'BANG FUAD', true],
-  ['628989513496', ':v', true],
-  ['628989513496', 'BANG FUAD', true] //Ganti jd nomormu sama Namamu
+  ['628XXXXXXXXXX', 'WH MODS DEV', true],
+  ['-', 'isi_kalau_ada', true],
+  ['-', 'isi_kalau_ada', true],
+  ['-', 'isi_kalau_ada', true] //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -109,22 +109,21 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '「 NeKo-Botzz」' //Main Watermark
-global.wm2 = 'NEKO-BOTZZ'
-global.wm3 = '⫹⫺ NeKo-Botzz '
+global.wm = '                「 WH-MODS-BOT-V1 」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘WH-MODS-BOT-V1 ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '⫹⫺ WH-MODS-BOT-V1 '
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `⫹⫺ WhatsApp Bot | NeKo-Botzz`
-global.author = '「 NeKo-Botzz 」'
-
+global.titlebot = `⫹⫺ WhatsApp Bot | By WH-MODS-DEV`
+global.author = '                「 WH-MODS-BOT-V1  あ」'
 
 
 /*============== LOGO ==============*/
 global.thumb = 'https://i.ibb.co/5KG7wmH/20220927-214127.jpg' //Main Thumbnail
-global.imagebot = 'https://i.ibb.co/5KG7wmH/20220927-214127.jpg'
+global.imagebot = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
-global.thumbs = ['https://i.ibb.co/5KG7wmH/20220927-214127.jpg']
-global.fla = 'https://i.ibb.co/5KG7wmH/20220927-214127.jpg'
+global.thumbs = ['https://i.ibb.co/XZrK6yQ/transformers.jpg']
+global.fla = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
 global.flaaa2 = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
@@ -134,7 +133,7 @@ global.flaaa2 = [
 
 /*============== HENTAI ==============*/ 
 //OPTIONS
-global.optsnsfw = false // Untuk mematikan Dan Menyalakan Nsfw
+global.optsnsfw = true // Untuk mematikan Dan Menyalakan Nsfw
 global.premnsfw = true // Nsfw Mode Premium?
 
 
@@ -190,9 +189,8 @@ global.htjava = '⫹⫺'    //hiasan Doang :v
 global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
-global.stickpack = 'Neko-Botzz '
-global.stickauth = `BANG FUAD '
-`
+global.stickpack = 'WH-MODS-BOT-V1'
+global.stickauth = `By ©WH-MODS-DEV`
 
 global.multiplier = 38 // The higher, The harder levelup
 
